1bf3707c83d901686ec79162602ef6c49aabb7a6
Анатолий <Chepanov199287@gmail.com>
2023-04-05 22:47:51 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillFactory_5_2_17
+{
+    class Program
+    {
+        static void ShowArray(int[] array, bool priznak = true)
+        {
+            if (priznak)
+            {
+                Array.Sort(array);
+            }
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+                Console.ReadKey();
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] array = { 6, 10, 5 };
+            ShowArray(array);
+        }
+    }
+}
